2e90d9a2578f5476282651d9015a8303a83e3fc0
Nathan Nguyen <31893431+Doomking36@users.noreply.github.com>
2024-06-27 13:39:40 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Block Redirects and New Tabs
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  Block redirects and new tabs, allow user to decide, with a list of trusted domains
 // @author       Your Name
 // @match        *://*/*
@@ -81,4 +81,45 @@
             return originalLocationReplace.call(window.location, url);
         }
     };
+
+    // Monitor changes to the href attribute of the window location
+    const originalSetHref = Object.getOwnPropertyDescriptor(window.location.__proto__, 'href').set;
+    Object.defineProperty(window.location.__proto__, 'href', {
+        set: function(url) {
+            if (!isTrustedDomain(url)) {
+                if (confirm(`The website is attempting to redirect to:\n\n${url}\n\nDo you want to proceed?`)) {
+                    originalSetHref.call(window.location, url);
+                }
+            } else {
+                originalSetHref.call(window.location, url);
+            }
+        }
+    });
+
+    // Monitor changes to the location through other properties
+    ['assign', 'replace'].forEach(function(method) {
+        const originalMethod = window.location[method];
+        window.location[method] = function(url) {
+            if (!isTrustedDomain(url)) {
+                if (confirm(`The website is attempting to redirect to:\n\n${url}\n\nDo you want to proceed?`)) {
+                    return originalMethod.call(window.location, url);
+                }
+            } else {
+                return originalMethod.call(window.location, url);
+            }
+        };
+    });
+
+    // Monitor immediate location changes
+    setInterval(() => {
+        if (!isTrustedDomain(window.location.href)) {
+            const currentHref = window.location.href;
+            if (confirm(`The website is attempting to redirect to:\n\n${currentHref}\n\nDo you want to proceed?`)) {
+                window.location.href = currentHref;
+            } else {
+                window.stop();
+            }
+        }
+    }, 1000);
+
 })();
