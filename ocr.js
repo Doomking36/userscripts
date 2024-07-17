e5613fbf5e15330bf37b02bc6c69ddda6797a8d9
Nathan Nguyen <31893431+Doomking36@users.noreply.github.com>
2024-07-16 21:35:32 -0500

    Create ocr.js

---
+++ ocr.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name  OCR Image Content
+// @version  0.1
+// @description  Extract text from selected area using OCR
+// @author  Doomking
+// @match  *://*/*
+// @require  https://unpkg.com/tesseract.js@v2.1.0/dist/tesseract.min.js
+// @require  https://html2canvas.hertzen.com/dist/html2canvas.min.js
+// @grant  GM_setClipboard
+// @grant  GM_addStyle
+// ==/UserScript==
+
+(function() {
+  'use strict';
+  
+})();
